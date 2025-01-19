3bd56c52e5fcd13f7c53e123ee63c007be70afbf
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-19 09:46:34 +0800

    refactor: change HTML structure for login page

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php
 include 'config.php';
 
@@ -21,21 +22,78 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <title>Login</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login - Tata Usaha</title>
+    <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+    rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/assets/css/tailwind.output.css" />
+    <script
+    src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+    defer
+    ></script>
+    <script src="/assets/js/init-alpine.js"></script>
 </head>
 <body>
-    <h2>Login</h2>
-    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
-    <form method="POST" action="">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
-    <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
+    <form method="POST"  class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+      <div
+      class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
+      >
+      <div class="flex flex-col overflow-y-auto md:flex-row">
+          <div class="h-32 md:h-auto md:w-1/2">
+            <img
+            aria-hidden="true"
+            class="object-cover w-full h-full dark:hidden"
+            src="https://blog-edutore-partner.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2021/05/25122840/Profesi-Bagian-Tata-Usaha_610-x-610-px.png"
+            alt="Office"
+            />
+            <img
+            aria-hidden="true"
+            class="hidden object-cover w-full h-full dark:block"
+            src="../assets/img/login-office-dark.jpeg"
+            alt="Office"
+            />
+        </div>
+        <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <div class="w-full">
+              <h1
+              class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
+              >
+              Login
+          </h1>
+          <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Username</span>
+            <input
+            name="username"
+            autofocus="autofocus"
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-green-400 focus:outline-none focus:shadow-outline-green dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            placeholder="johndoe"
+            />
+        </label>
+        <label class="block mt-4 text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Password</span>
+            <input
+            name="password"
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-green-400 focus:outline-none focus:shadow-outline-green dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            placeholder="***************"
+            type="password"
+            />
+        </label>
+
+        <button type="submit"
+        style="background-color: #38a169; border: none; border-radius: 0.5rem; text-align: center; display: inline-block;"
+        class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-green-700 border border-transparent rounded-lg active:bg-green-700 hover:bg-green-700 focus:outline-none focus:shadow-outline-green"       >
+        Log in
+    </button>
+
+</div>
+</div>
+</div>
+</div>
+</div>
 </body>
 </html>
