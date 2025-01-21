4cd2ca04a1c784f66d155c3384088af3c4eeea82
Raihan Alkawsar <tlod9906@gmail.com>
2025-01-21 20:31:55 +0800

    refactor: change database name in config.php

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'school_xii_tatausaha');
+// define('DB_NAME', 'school_xii_tatausaha');
+define('DB_NAME', 'school_laravel_tata_usaha');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
@@ -31,3 +32,4 @@ function check_login() {
         exit();
     }
 }
+
